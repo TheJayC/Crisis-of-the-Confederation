d8776a4ca2af8c3ff71fee5b570dc09a9c463370
Cjay <Janmw3@Hotmail.com>
2018-04-20 18:01:12 -0400

    The mod files

---
+++ Crisis of The Confederation/common/defines/Prestige Based Merchant Republic Elections.lua
@@ -0,0 +1,4 @@
+	NDefines.NEconomy.DOGE_SUCC_RANDOM_FACTOR = 0									-- 500 The random Respect factor on actual Doge succession.
+	NDefines.NEconomy.PATRICIAN_CAMPAIGN_FUND_FACTOR = 2.5									-- 5 The effect of money in the Campaign Fund on Doge elections (money * factor).
+	NDefines.NEconomy.PATRICIAN_PRESTIGE_RESPECT_FACTOR = 5									-- 2 The effect of Prestige on the Respect value for Doge elections (prestige * factor).
+	NDefines.NEconomy.PATRICIAN_AGE_RESPECT_FACTOR = 0.125									-- 1 The effect of Age on the Respect value for Doge elections (age * age * factor).
